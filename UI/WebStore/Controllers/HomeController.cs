4af7710591050717fb7c985d5a2daca41d0e2dce
deepar57 <47324708+deepar57@users.noreply.github.com>
2020-06-24 20:13:54 +0300

    01

---
+++ UI/WebStore/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Throw(string id) => throw new ApplicationException(id ?? "Error!");
+
+        public IActionResult Blog() => View();
+        
+        public IActionResult BlogSingle() => View();
+        
+        public IActionResult Cart() => View();
+        
+        public IActionResult CheckOut() => View();
+        
+        public IActionResult ContactUs() => View();
+        
+        public IActionResult Login() => View();
+        
+        public IActionResult ProductDetails() => View();
+        
+        public IActionResult Shop() => View();
+    }
+}
