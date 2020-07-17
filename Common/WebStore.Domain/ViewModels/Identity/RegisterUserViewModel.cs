bb992e2f047021dc9e08e8572009ccbd408f7066
deepar57 <deepar@yandex.ru>
2020-07-17 17:09:15 +0300

    part-2

--- Common/WebStore.Domain/ViewModels/Identity/RegisterUserViewModel.cs
+++ Common/WebStore.Domain/ViewModels/Identity/RegisterUserViewModel.cs
@@ -1,24 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebStore.Domain.ViewModels.Identity
 {
-    public class RegisterUserViewModel
-    {
-        [Required]
-        [MinLength(3, ErrorMessage = "Минимальная длина 3 символа")]
-        [MaxLength(256)]
-        [Display(Name = "Имя пользователя")]
-        public string UserName { get; set; }
+	public class RegisterUserViewModel
+	{
+		[Required]
+		[MinLength(3, ErrorMessage = "Минимальная длина 3 символа")]
+		[MaxLength(256)]
+		[Display(Name = "Имя пользователя")]
+		[Remote("IsNameFree", "Account")]
+		public string UserName { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Пароль")]
-        public string Password { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Пароль")]
+		public string Password { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Подтверждение пароля")]
-        [Compare(nameof(Password))]
-        public string ConfirmPassword { get; set; }
-    }
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Подтверждение пароля")]
+		[Compare(nameof(Password))]
+		public string ConfirmPassword { get; set; }
+	}
 }
